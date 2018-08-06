853442138960cb740b7c904bf8a9cdcb96742c40
美滋滋 <39560832+liuxi821@users.noreply.github.com>
2018-08-06 22:31:19 +0800

    Update h.hpp

--- Sort/Quick sequencing/h.hpp
+++ Sort/Quick sequencing/h.hpp
@@ -19,3 +19,27 @@ int GetMidIdx(int *array, int left, int right)
 			return mid;
 	}
 }
+
+
+//三数取中
+int GetMidData(int *array, int left, int right)
+{
+	int mid = left + ((right - left) >> 1);
+	if (array[left] < array[right - 1])
+	{
+		if (array[left]>array[mid])
+			return array[left];
+		else if (array[right - 1] < array[mid])
+			return array[right - 1];
+		return array[mid];
+	}
+	else /*if (array[left]>array[right - 1])*/
+	{
+		if (array[mid] > array[left])
+			return array[left];
+		else if (array[mid] < array[right - 1])
+			return array[right - 1];
+		else
+			return array[mid];
+	}
+}
